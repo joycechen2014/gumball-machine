05480d7c677677562965266260efc19e9be2e83c
Jinfeng Chen <jinfeng.chen@sjsu.edu>
2019-09-21 23:05:24 -0700

    version 1.0

--- src/GumballMachine.java
+++ src/GumballMachine.java
@@ -1,47 +1,89 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class GumballMachine
 {
 
     private int num_gumballs;
     private boolean has_quarter;
+    private Map<Integer,Integer> coin_type;
+    private int price;
+    private int curr_money;
 
-    public GumballMachine( int size )
+    public GumballMachine(int size , Set<Integer> coin_type, int price)
     {
+        this.coin_type = new HashMap<Integer, Integer>();
         // initialise instance variables
         this.num_gumballs = size;
-        this.has_quarter = false;
+        for(Integer key : coin_type) {    // delete coins;
+            this.coin_type.put(key,0);
+        }
+       // this.coin_type.addAll(coin_type);
+        this.price = price;
+        this.curr_money = 0;
     }
 
     public void insertQuarter(int coin)
     {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else
-            this.has_quarter = false ;
+        System.out.println("You insert ￠" + coin + ".");
+         if(this.coin_type.containsKey(coin)) {
+             this.coin_type.put(coin,this.coin_type.get(coin) + 1);
+             this.curr_money += coin;
+         }
+         else {
+             System.out.println("We can not accept ￠" + coin);
+             System.out.println("You have ejected 1" + " coin of " + coin);
+             this.ejectQuarter();
+             this.curr_money = 0;
+         }
     }
 
     public void turnCrank()
     {
-        if ( this.has_quarter )
+        if ( this.curr_money == this.price)
         {
             if ( this.num_gumballs > 0 )
             {
                 this.num_gumballs-- ;
-                this.has_quarter = false ;
+                this.curr_money = 0 ;
+                for(Integer key : coin_type.keySet()) {    // delete coins;
+                    coin_type.put(key,0);  //delete coins;
+                }
                 System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
             }
             else
             {
-                System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
+                System.out.println( "Sorry, we are out of gumball and your money will reject!" ) ;
+                ejectQuarter();
             }
         }
         else
         {
-            System.out.println( "Please insert a quarter" ) ;
+            int money_still_need = price - curr_money;
+            System.out.println("You still need ￠" + money_still_need);
         }
+    }
+
+    public void ejectQuarter() {
+        for(Integer key : this.coin_type.keySet()) {
+            if(this.coin_type.get(key) != 0)
+            System.out.println("You have ejected " + this.coin_type.get(key) + " coin of ￠" + key);
+            this.coin_type.put(key,0);  //delete coins;
+        }
+        this.curr_money = 0;
+
     }
 
     @Override
     public String toString() {
-        return "Gumball Machine has " + num_gumballs + " gumballs now!!!";
+
+        String string2 = new String();
+        for(int key : this.coin_type.keySet()) {
+            string2 = string2 + " ￠" + key + " ";
+        }
+        return "Gumball Machine has " + num_gumballs + " gumballs now!\n"  +
+                "The cost of a Gumball is ￠" + this.price +
+                " and we accept " + string2 ;
     }
 }
--- src/GumballMachineTest.java
+++ src/GumballMachineTest.java
@@ -1,11 +1,42 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class GumballMachineTest {
-    public static void main(String [ ] args) {
-        GumballMachine gumballmachine;
-        gumballmachine = new GumballMachine(5);
-        System.out.println(gumballmachine);
-        gumballmachine.insertQuarter(25);
-        gumballmachine.turnCrank();
-        System.out.println(gumballmachine);
+    public static void main(String[] args) {
+
+        Set<Integer> coins_type_quarter = new HashSet<Integer>(){
+            {
+                add(25);
+            }
+        };
+        Set<Integer> coins_type_any = new HashSet<Integer>(){
+            {
+                add(50);
+                add(25);
+                add(10);
+                add(1);
+            }
+        };
+        GumballMachine gumballmachine_25 =  new GumballMachine(5,coins_type_quarter,25);
+
+        System.out.println(gumballmachine_25);
+        gumballmachine_25.insertQuarter(25);
+        gumballmachine_25.turnCrank();
+        System.out.println(gumballmachine_25);
+
+        System.out.println("=====================================");
+
+        GumballMachine gumballmachine_50 =  new GumballMachine(5,coins_type_quarter,50);
+        System.out.println(gumballmachine_50);
+        gumballmachine_50.insertQuarter(25);
+        gumballmachine_50.turnCrank();
+        System.out.println(gumballmachine_50);
+        System.out.println("=====================================");
+        GumballMachine gumballmachine_50_any =  new GumballMachine(5,coins_type_any,50);
+        System.out.println(gumballmachine_50_any);
+        gumballmachine_50_any.insertQuarter(10);
+        gumballmachine_50_any.turnCrank();
+        System.out.println(gumballmachine_50_any);
 
     }
 }
